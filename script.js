2da38158964589e64e931ee2cf7a617f8642d476
magalhaesf <160252530+magalhaesf@users.noreply.github.com>
2025-02-18 07:41:48 -0300

    Algoritmo em JavaScript

---
+++ script.js
@@ -0,0 +1,18 @@
+/*var aluno = "Fernanda Magalhães";
+alert(aluno);
+console.log(aluno);
+prompt("Digite o nome da sua mãe");
+console.log(prompt("Digite o nome da sua mãe"));
+;*/
+
+var nome = prompt("Digite seu nome completo");;
+var endereco = prompt("Digite seu endereço");;
+var cartao = prompt("Digite o número de seu cartão");;
+var senha = prompt("Digite a senha");;
+
+console.log(nome);
+console.log(endereco);
+console.log(cartao);
+console.log(senha);
+
+
